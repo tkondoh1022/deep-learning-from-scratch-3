311bdf6ec8e10dddb2de3610785d4291ae28ab4f
Takeo Kondoh <tkondoh1022@gmail.com>
2022-01-17 22:17:34 +0900

    steps

---
+++ steps/s04.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+
+class Variable:
+    def __init_(self, data):
+        self.data = data
+
+
+class Function:
+    def __call__(self, input):
+        x = input.data
+        y = self.forward(x)
+        output = Variable(y)
+        self.input = input
+        self.output = output
+        return output
+
+    def forward(self, x):
+        raise NotImplementedError()
+
+
+class Exp(Function):
+    def forward(self, x):
+        return np.exp(x)
+
+
+def numerical_diff(f, c, eps=1e-4):
+    x0 = Variable(x.data - eps)
+    x1 = Variable(x.data + eps)
+    y0 = f(x0)
+    y1 = f(x1)
+    return (y1.data - y0.data) / (2 * eps)
+
+
+f = Square()
+x = Variable(np.array(2.0))
+dy = numerical_diff(f, x)
+print(dy)
