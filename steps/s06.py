37be88b36522d79291baccff5c92bf7dd792d8be
Takeo Kondoh <tkondoh1022@gmail.com>
2022-01-25 11:18:40 +0900

    steps/s06.py

--- steps/s06.py
+++ steps/s06.py
@@ -1,3 +1,12 @@
 import numpy as np
 
-class
+
+class Variable:
+    def __init__(self, data):
+        self.data = data
+        self.grad = None
+
+
+class Function:
+    def __call__(self, input):
+        x = input.data
