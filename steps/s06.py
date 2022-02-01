240ddab84c50f4c501bd34cd496604892b5bda80
Takeo Kondoh <tkondoh1022@gmail.com>
2022-02-01 15:49:26 +0900

    update

--- steps/s06.py
+++ steps/s06.py
@@ -14,3 +14,47 @@ class Function:
         output = Variable(y)
         self.input = input
         return output
+
+    def forward(self, x):
+        raise NotImplementedError()
+
+    def backword(self, gy):
+        raise NotADirectoryError()
+
+
+class Square(Function):
+    def forward(self, x):
+        y = x ** 2
+        return y
+
+    def backword(self, gy):
+        x = self.input.data
+        gx = 2 * x * gy
+        return gx
+
+
+class Exp(Function):
+    def forward(self, x):
+        y = np.exp(x)
+        return y
+
+    def backword(self, gy):
+        x = self.input.data
+        gx = np.exp(x) * gy
+        return gy
+
+
+A = Square()
+B = Exp()
+C = Square()
+
+x = Variable(np.array(0.5))
+a = A(x)
+b = B(a)
+y = C(b)
+
+y.grad = np.array(1.0)
+b.grad = C.backword(y.grad)
+a.grad = B.backword(b.grad)
+x.grad = A.backword(a.grad)
+print(x.grad)
